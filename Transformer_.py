f94bbc06d4b766430c8a165e77bdcbbda999a722
Therealryanmagee <135673156+Therealryanmagee@users.noreply.github.com>
2023-08-28 15:04:44 -0500

    -Transformer.py

--- Transformer_.py
+++ Transformer_.py
@@ -1,10 +1,30 @@
-# Main ML framework to be built here.
 import math
+import os
+from tempfile import TemporaryDirectory
+from typing import Tuple
+
 import torch
-import setuptools
+from torch import nn, Tensor
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import dataset
 
-class ml_transformer(torch.nn.Module):
-    def __init__(self):
+class TramsformerModel(nn.Module):
+    def __init__(self, ntoken: int, d_model: int, nhead: int, d_head: int, nlayers: int, dropout: float = 0.5):
         super().__init__()
+        self.model_type = 'transformer'
+        self.pos_encoder = TransformerEncoder(d_model, dropout)
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, d_head, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.embedding = nn.Embedding(ntoken, d_model)
+        self.d_model = d_model
+        self.linear = nn.Linear(d_model, ntoken)
 
-        self.encoder = torch.nn.TransformerEncoderLayer()
+        self.init_weights()
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.linear.weight.data.uniform_(-initrange, initrange)
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-initrange, initrange)
+
+

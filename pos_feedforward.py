ebc4d68b0417ce97b691911e9dd3e6316843c6b4
Therealryanmagee <135673156+Therealryanmagee@users.noreply.github.com>
2023-08-29 12:05:32 -0500

    -Transformer.py

---
+++ pos_feedforward.py
@@ -0,0 +1,33 @@
+
+
+# The PositionWiseFeedForward class extends PyTorch’s nn.Module and implements a position-wise feed-forward network.
+# The class initializes with two linear transformation layers and a ReLU activation function.
+# The forward method applies these transformations and activation function sequentially to compute the output.
+# This process enables the model to consider the position of input elements while making predictions.
+
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model, d_ff):
+        super(PositionWiseFeedForward, self).__init__()
+        self.fc1 = nn.Linear(d_model, d_ff)
+        self.fc2 = nn.Linear(d_ff, d_model)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        return self.fc2(self.relu(self.fc1(x)))
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_seq_length):
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(max_seq_length, d_model)
+        position = torch.arange(0, max_seq_length, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        self.register_buffer('pe', pe.unsqueeze(0))
+
+    def forward(self, x):
+        return x + self.pe[:, :x.size(1)]
